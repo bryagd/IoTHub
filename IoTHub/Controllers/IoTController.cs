a89de7b9fa6d45ed939890362449d2860d9de9e2
Gregory Bryant <bryagd@gmail.com>
2016-01-17 00:12:19 -0500

    Initial Commit

---
+++ IoTHub/Controllers/IoTController.cs
@@ -0,0 +1,22 @@
+﻿using IoT.Service.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IoT.Service.Controllers
+{
+    public class IoTController : ApiController
+    {
+        // GET: api/IoT
+        public string Get()
+        {  
+           return Request.GetRemoteClientIpAddress();
+        }
+
+
+ 
+    }
+}
---
+++ IoTHub/Extensions/HttpRequestMessageExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Service.Extensions
+{
+ public static   class HttpRequestMessageExtension
+    {
+        private const string HttpContext = "MS_HttpContext";
+        public static string GetRemoteClientIpAddress(this HttpRequestMessage request)
+        {
+            if (request.Properties.ContainsKey(HttpContext))
+            {
+                dynamic ctx = request.Properties[HttpContext];
+                if (ctx != null)
+                {
+                    return ctx.Request.UserHostAddress;
+                }              
+            }
+
+            return null;
+        }
+    }
+}
